343bd5713b4f91b48af92e8c781f13ed9afbf5b0
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-05 20:55:47 +0900

    [setting] 프로젝트 생성, prettier, eslint, tailwindcss, supabase

---
+++ src/utils/supabase/server.ts
@@ -0,0 +1,17 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+
+export const createClient = (
+  cookies: Partial<{ [key: string]: string | undefined }>,
+) => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        get(name: string) {
+          return cookies[name];
+        },
+      },
+    },
+  );
+};
