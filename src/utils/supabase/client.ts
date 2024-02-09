6bb5bc78bc83063f14fb3ff64f185d7da20f0662
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-09 20:48:02 +0900

    [format] 데이터베이스 타입 추가

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,8 +1,8 @@
+import { Database } from '@/types/supabase';
 import { createBrowserClient } from "@supabase/ssr";
-// import { Database } from '@/types/supabase';
-// createClient<Database>
+
 export const createClient = () =>
-  createBrowserClient(
+  createBrowserClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
   );
--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -1,7 +1,8 @@
+import { Database } from '@/types/supabase';
 import { createServerClient } from "@supabase/ssr";
 
 export const createClient = (cookies: Partial<{ [key: string]: string }>) => {
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
