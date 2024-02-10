a8c894335a7143ffa9a8f7e46555e762f28895ed
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2024-02-10 21:47:24 +0900

    [feat] 무한스크롤

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,28 +1,48 @@
 import PostCard from '@/components/PostCard';
 import { createClient } from '@/utils/supabase/client';
-import { useQuery } from '@tanstack/react-query';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 
 const supabase = createClient();
 
 export default function Home() {
-  const { data: posts } = useQuery({
+  const { ref, inView } = useInView();
+  const {
+    data: postPages,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery({
     queryKey: ['posts'],
-    queryFn: async () => {
+    queryFn: async ({ pageParam }) => {
       const { data } = await supabase
         .from('Post')
         .select('*')
-        .order('created_at', { ascending: false });
+        .order('created_at', { ascending: false })
+        .range(pageParam, pageParam + 9); // 총 10개의 글들을 한 페이지로 가져오겠다.
 
-      if (!data) return [];
-      return data;
+      if (!data) return { posts: [], nextPage: null };
+      return {
+        posts: data,
+        nextPage: data.length === 10 ? pageParam + 10 : null,
+      };
     },
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.nextPage,
   });
 
+  useEffect(() => {
+    if (inView && hasNextPage) fetchNextPage();
+  }, [inView, hasNextPage]);
+
   return (
-    <main className='h-[2000px]'>
+    <main className='flex flex-col'>
       <div className='container mx-auto grid grid-cols-2 gap-x-4 gap-y-6 lg:gap-x-7 lg:gap-y-12 px-4 pb-24 pt-20'>
-        {posts?.map((post) => <PostCard key={post.id} {...post} />)}
+        {postPages?.pages
+          .flatMap((page) => page.posts)
+          .map((post) => <PostCard key={post.id} {...post} />)}
       </div>
+      <div ref={ref} />
     </main>
   );
 }
